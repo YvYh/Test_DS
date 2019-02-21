205c695089c12ee9d258eb9b6e99de4e825b9f0b
Hong <ivoneyh124@gmail.com>
2019-02-21 19:05:16 +0100

    add comments

--- clustering.py
+++ clustering.py
@@ -13,6 +13,23 @@ from sklearn.cluster import KMeans
 
 
 def vectorizer(data, list_roads):
+    """
+    Creat one-hot vector for each address in the data
+    
+    Parameters
+    ----------
+    data : (N,) list
+        Input list. Each ligne is a dictionary containing a list of roads
+    list_roads : (N,) list
+        List of all appeared road names
+
+
+    Returns
+    -------
+    vect : DataFrame
+        The matix of all address in data.
+    
+    """
     vect = pd.DataFrame()
     length = len(list_roads) 
     for i in range(len(data)):
@@ -37,6 +54,30 @@ def vectorizer(data, list_roads):
 
 
 def n_cluster(vect, n_min = 5, n_max = 0, n_point = 5):
+    """
+    Choose the best number of cluster in defined range or not
+    
+    Parameters
+    ----------
+    vect : DataFrame
+        The matix of all address in data.
+    n_min : int
+        minimum number of clusters, default value is 5
+
+    n_max: int 
+        maximum number of clusters, default value is number of data/number of 
+        point in each cluster
+    n_point: 
+        how many point in each cluster, default value is 5
+
+
+    Returns
+    -------
+    n : int
+        The number of cluster when clustering model will has the best 
+        performance
+    
+    """
     distance = []
     k = []
     #when n_max isn't defined or n_max<n_min
@@ -67,32 +108,43 @@ def n_cluster(vect, n_min = 5, n_max = 0, n_point = 5):
 
 # =============================================================================
 # main function
+# Perform a clustering based on either the location or characteristics of 
+# bike stations.
 # =============================================================================
-def main(auto=True, n_cluster = 5, n_min = 5, n_max = 0, n_point = 5):
-    #read json file
-    file = input("Json file:")
-    file = 'Brisbane_CityBike.json'
-    with open(file) as f:
-        data = json.load(f)
-    
-    # get the list of all appeared road names
-    list_roads = []
-    for i in range(len(data)):
-        roads = data[i]['address']
-        symbol_special=[' /  ',' (',')',' / ']
-        for s in symbol_special:
-            roads = roads.replace(s,'/')
-        roads = re.split('/',roads)
-        data[i]['roads'] = roads
-        list_roads.extend(roads)
-    list_roads = list(set(list_roads))
-    list_roads = [x for x in list_roads if x != '']
-    
-    #vectorize address
-    vect = vectorizer(data, list_roads)
-    
-    if (auto):
-        n_cluster = n_cluster(vect, n_min, n_max, n_point)
-    model = KMeans(n_clusters=5)
-    data2 = pd.DataFrame.from_dict(data)
-    data2['label'] = model.fit_predict(vect)
+
+#read json file
+file = input("Json file:")
+file = 'Brisbane_CityBike.json'
+with open(file) as f:
+    data = json.load(f)
+
+# get the list of all appeared road names
+list_roads = []
+for i in range(len(data)):
+    roads = data[i]['address']
+    symbol_special=[' /  ',' (',')',' / ']
+    for s in symbol_special:
+        roads = roads.replace(s,'/')
+    roads = re.split('/',roads)
+    data[i]['roads'] = roads
+    list_roads.extend(roads)
+list_roads = list(set(list_roads))
+list_roads = [x for x in list_roads if x != '']
+
+#vectorize address
+vect = vectorizer(data, list_roads)
+
+auto=True
+if (auto):
+    n_min = 5
+    n_max = 0
+    n_point = 5
+    n_cluster = n_cluster(vect, n_min, n_max, n_point)
+else:
+    n_cluster = 5
+
+if (auto):
+    n_cluster = n_cluster(vect, n_min, n_max, n_point)
+model = KMeans(n_clusters=5)
+data2 = pd.DataFrame.from_dict(data)
+data2['label'] = model.fit_predict(vect)
