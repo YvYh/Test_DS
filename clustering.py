357fa4bb0870047594e0ebc0b2051bff2ad38076
Hong <ivoneyh124@gmail.com>
2019-02-21 19:14:53 +0100

    main function: write result to csv

--- clustering.py
+++ clustering.py
@@ -10,6 +10,29 @@ import json
 import re
 from scipy.spatial.distance import hamming
 from sklearn.cluster import KMeans 
+"""
+Variables
+----------
+auto: boolean
+    detemine number of clusters automatically or not
+    
+n_cluster: int
+    user-defined number of clusters
+
+n_min : int
+    minimum number of clusters, default value is 5
+
+n_max: int 
+    maximum number of clusters, default value is number of data/number of 
+    point in each cluster
+n_point: 
+    how many point in each cluster, default value is 5
+"""
+auto=True
+n_min = 5
+n_max = 0
+n_point = 5
+n_cluster = 5
 
 
 def vectorizer(data, list_roads):
@@ -55,7 +78,7 @@ def vectorizer(data, list_roads):
 
 def n_cluster(vect, n_min = 5, n_max = 0, n_point = 5):
     """
-    Choose the best number of cluster in defined range or not
+    Choose the best number of cluster in given(or default) range
     
     Parameters
     ----------
@@ -112,13 +135,17 @@ def n_cluster(vect, n_min = 5, n_max = 0, n_point = 5):
 # bike stations.
 # =============================================================================
 
+
 #read json file
+print("== 1/6 Read data")
 file = input("Json file:")
-file = 'Brisbane_CityBike.json'
+#file = 'Brisbane_CityBike.json'
 with open(file) as f:
     data = json.load(f)
 
+
 # get the list of all appeared road names
+print("== 2/6 Find all appeared road names")
 list_roads = []
 for i in range(len(data)):
     roads = data[i]['address']
@@ -132,19 +159,23 @@ list_roads = list(set(list_roads))
 list_roads = [x for x in list_roads if x != '']
 
 #vectorize address
+print("== 3/6 Vectorize address")
 vect = vectorizer(data, list_roads)
 
-auto=True
+#determine n_cluster
+print("== 4/6 Determine n_cluster")
 if (auto):
-    n_min = 5
-    n_max = 0
-    n_point = 5
     n_cluster = n_cluster(vect, n_min, n_max, n_point)
-else:
-    n_cluster = 5
 
-if (auto):
-    n_cluster = n_cluster(vect, n_min, n_max, n_point)
-model = KMeans(n_clusters=5)
-data2 = pd.DataFrame.from_dict(data)
-data2['label'] = model.fit_predict(vect)
+
+print("== 5/6 Clustering")
+model = KMeans(n_cluster)
+df = pd.DataFrame.from_dict(data)
+df['label'] = model.fit_predict(vect)
+df = df.drop("roads", 1)
+result = df.sort_values("label")
+
+
+#write result to csv files
+print("== 6/6 Output")
+result.to_csv("result.csv")
